0269c84d3ef3e5174b0aeb0899e6cd31e8dda420
Edson Ribeiro <edsonribeirojr95@gmail.com>
2025-04-11 01:43:32 -0300

    feat: alter link to new sites

--- js/uuid.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('gerar_uuidv4').addEventListener('click', () => {
-    document.getElementById('uuidv4_gerado').value = crypto.randomUUID();
-});
-
-document.getElementById('btn-copy').addEventListener('click', () => {
-    let uuidv4_gerado = document.getElementById("uuidv4_gerado").value;
-    navigator.clipboard.writeText(uuidv4_gerado);
-});
